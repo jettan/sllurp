e4891925c099b4b0041e7741f05d5ed2be7579ee
Jethro Tan <jethrotan20@gmail.com>
2015-07-12 16:26:13 +0200

    Make postprocessing scripts process bwr too.

--- postprocess.py
+++ postprocess.py
@@ -12,6 +12,7 @@ else:
 	total = 0
 	m = 0
 	r = 0
+	s = 0
 	eof_line = "EOF was not reached!"
 	for i in range(0,len(lines)):
 		if "Resending" in lines[i]:
@@ -22,13 +23,17 @@ else:
 			total += 1
 		if "Result=0" in lines[i]:
 			success += 1
+		if "'Result': 0" in lines[i]:
+			s+=1
 		if "EOF reached" in lines[i]:
 			eof_line = lines[i]
 		else:
 			continue
 	
-	num_words =  int(str.split(lines[1])[3])
-	
+	try:
+		num_words =  int(str.split(lines[1])[3])
+	except:
+		num_words = 1
 #	print "-------------------------------------------------------"
 #	print "Filename: " + sys.argv[1]
 #	print "Total number of words in transfer: " + str(num_words)
@@ -48,25 +53,34 @@ else:
 #		print "Transfer goodput: " + str((num_words*2)/runtime) + " bytes/sec"
 #		print "Transfer throughput: " + str((num_words*2)/runtime + (m*2*2)/runtime) + " bytes/sec"
 #	print "-------------------------------------------------------"
-
-	payload = int(sys.argv[1][3:len(sys.argv[1])-6])
-	messages_sent = m
-	messages_resent = r
-	resend_ratio = float(r)/(r+m)
+	try:
+		payload = int(sys.argv[1][3:len(sys.argv[1])-6])
+		messages_sent = m
+		messages_resent = r
+		resend_ratio = float(r)/(r+m)
+		success_reports = success
+		total_reports = total
+		efficiency = float(success)/(total)
+		success_opm = float(success)/(r+m)
+		total_opm = float(total)/(r+m)
+		
+		time_per_message = runtime/(r+m)
+		messages_per_second = 1/time_per_message
+		time_per_op = runtime/total
+		success_ops = success_opm*messages_per_second
+		total_ops = total_opm*messages_per_second
+		goodput = (num_words*2)/runtime
+		throughput = goodput + (m*2*2)/runtime
+		print "%d, %d, %d, %f, %d, %d, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f" % ( payload, messages_sent, messages_resent, resend_ratio, success_reports, total_reports, efficiency, 
+		success_opm, total_opm, runtime, time_per_message, messages_per_second, time_per_op, success_ops, total_ops, goodput, throughput)
+	except:
+		finger = len(sys.argv[1].split('_')) - 1
+		payload = int(sys.argv[1].split('_')[finger][0:len(sys.argv[1].split('_')[finger])-4])
+		success_reports = s
+		total_reports = total
+		efficiency = float(s)/total
+		throughput = (float(s)*payload*2)/10
+		ops = float(s)/10
+		
+		print "%d, %d, %d, %f, %f, %f" % (payload, total_reports, success_reports, efficiency, throughput, ops)
 	
-	success_reports = success
-	total_reports = total
-	efficiency = float(success)/(total)
-	success_opm = float(success)/(r+m)
-	total_opm = float(total)/(r+m)
-	
-	time_per_message = runtime/(r+m)
-	messages_per_second = 1/time_per_message
-	time_per_op = runtime/total
-	success_ops = success_opm*messages_per_second
-	total_ops = total_opm*messages_per_second
-	goodput = (num_words*2)/runtime
-	throughput = goodput + (m*2*2)/runtime
-	
-	print "%d, %d, %d, %f, %d, %d, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f" % ( payload, messages_sent, messages_resent, resend_ratio, success_reports, total_reports, efficiency, 
-	success_opm, total_opm, runtime, time_per_message, messages_per_second, time_per_op, success_ops, total_ops, goodput, throughput)
--- results/plot_data.m
+++ results/plot_data.m
@@ -1,13 +1,13 @@
 clear
 
-% PARAMETER TO PLOT.
+% Uncomment the parameter to plot.
 %s = 1; %payload,
 %s = 2; %messages_sent,
 %s = 3; %messages_resent
 %s = 4; %resend_ratio,
 %s = 5; %success_reports,
 %s = 6; %total_reports,
-s = 7;  %efficiency,
+%s = 7;  %efficiency,
 %s = 8;  %success_opm,
 %s = 9;% total_opm,
 %s = 10; %runtime,
@@ -17,29 +17,76 @@ s = 7;  %efficiency,
 %s = 14; %success_ops,
 %s = 15; %total_ops,
 %s = 16; %goodput,
-%s = 17; %throughput
+s = 17; %throughput
 
+% Read CSV files.
 d2 = csvread('wisent/20cm/result.csv',1);
 d3 = csvread('wisent/30cm/result.csv',1);
 d4 = csvread('wisent/40cm/result.csv',1);
 d5 = csvread('wisent/50cm/result.csv',1);
 d6 = csvread('wisent/60cm/result.csv',1);
 
+% Reshape matrices.
 r2 = reshape(d2,5,16,17);
 r3 = reshape(d3,5,16,17);
 r4 = reshape(d4,5,16,17);
 r5 = reshape(d5,5,16,17);
 r6 = reshape(d6,5,16,17);
 
+fontSize = 10;
+fontSizeAxes = 8;
+fontWeight = 'normal';
+figurePosition = [440 378 560 300];   % [x y width height]
+hFig = figure;
+set(hFig, 'Position', figurePosition)
+set(gcf,'Renderer','painters');
+
+data = [r2(:,:,s),r3(:,:,s),r4(:,:,s),r5(:,:,s),r6(:,:,s)];
+
+% Boxplot grouping stuff.
 a = [];
 for j=1:5,
     a = [a 1:16];
 end
-
-data = [r2(:,:,s),r3(:,:,s),r4(:,:,s),r5(:,:,s),r6(:,:,s)];
 l = [repmat({'20'},1,16), repmat({'30'},1,16),repmat({'40'},1,16),repmat({'50'},1,16),repmat({'60'},1,16)];
-h = boxplot(data,{a,l},'colors',repmat('rbgmk',1,16),'factorgap',[5 2],'labelverbosity','minor', 'factorseparator',1, 'plotstyle','compact', 'outliersize',0.1, 'medianstyle', 'line');
-legend(findobj(gca,'Tag','Box'),'60cm','50cm','40cm','30cm','20cm', 'Location','northwest');
-set(findobj(gca,'Type','text'),'FontSize',8)
-xlabel('Message payload size (words)')
-%ylabel('Efficiency')
+
+% Boxplot.
+b = boxplot(data,{a,l},'colors',repmat('rbgmk',1,16),'factorgap',[5 2], ...
+'labelverbosity','minor', 'factorseparator',1);
+set(findobj(get(b(1), 'parent'), 'type', 'text'), 'FontSize', fontSize, ...
+    'FontWeight', fontWeight);
+
+% Recolor boxes. 
+colors = repmat('kmgbr',1,16);
+h = findobj(gca,'Tag','Box');
+for j=1:length(h)
+   patch(get(h(j),'XData'),get(h(j),'YData'), colors(j),'FaceAlpha',1,'EdgeColor','none');
+end
+
+delete(findobj(gca,'Type','text','String','20'));
+delete(findobj(gca,'Type','text','String','30'));
+delete(findobj(gca,'Type','text','String','40'));
+delete(findobj(gca,'Type','text','String','50'));
+delete(findobj(gca,'Type','text','String','60'));
+
+h = xlabel('Message payload size (words)');
+set(h,'FontSize',fontSize);
+set(h,'fontweight', fontWeight);
+
+
+h = ylabel('Throughput [B/sec]');
+set(h,'FontSize',fontSize);
+set(h,'fontweight', fontWeight);
+
+
+% Turn on legend.
+h = legend(findobj(gca,'Tag','Box'),'60 cm','50 cm','40 cm','30 cm','20 cm', 'Location','northwest');
+set(h,'FontSize',fontSize);
+set(h,'fontweight', fontWeight);
+
+set(findobj('type','axes'),'fontsize',fontSizeAxes)
+
+txt=findobj(gca,'Type','text');
+set(txt,'FontSize',fontSize);
+set(txt,'VerticalAlignment','Middle');
+set(hFig, 'paperpositionmode', 'auto');
