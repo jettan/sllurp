4b0212e24e8a397d4479bef9564fbc03e3701490
Jethro Tan <jethrotan20@gmail.com>
2015-07-21 17:43:18 +0200

    Adjust script to plot new figures.

--- all.sh
+++ all.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "payload,messages_sent,messages_resent,resend_ratio,success_reports,total_reports,efficiency,success_opm,total_opm,runtime,time_per_message,messages_per_second,time_per_op,success_ops,total_ops,goodput,throughput" > result.csv
-#echo "payload, total, success, efficiency, throughput, ops" >> result.csv
+#echo "payload,messages_sent,messages_resent,resend_ratio,success_reports,total_reports,efficiency,success_opm,total_opm,runtime,time_per_message,messages_per_second,time_per_op,success_ops,total_ops,goodput,throughput" > result.csv
+echo "payload, total, success, efficiency, throughput, ops" >> result.csv
 for f in *.txt; do ../../../postprocess.py $f >> result.csv; done
--- results/plot_wisent.m
+++ results/plot_wisent.m
@@ -11,27 +11,27 @@ close all;
 %s = 7;  %efficiency,
 %s = 8;  %success_opm,
 %s = 9;% total_opm,
-%s = 10; %runtime,
+s = 10; %runtime,
 %s = 11; %time_per_message,
 %s = 12; %messages_per_second,
 %s = 13; %time_per_op,
 %s = 14; %success_ops,
 %s = 15; %total_ops,
 %s = 16; %goodput,
-s = 17; %throughput
+%s = 17; %throughput
 
 % Read CSV files.
-d2 = csvread('wisent/20cm/result.csv',1);
-d3 = csvread('wisent/30cm/result.csv',1);
-d4 = csvread('wisent/40cm/result.csv',1);
-d5 = csvread('wisent/50cm/result.csv',1);
-d6 = csvread('wisent/60cm/result.csv',1);
+d2 = csvread('wisent_random/20cm/result.csv',1);
+%d3 = csvread('wisent/30cm/result.csv',1);
+d4 = csvread('wisent_random/40cm/result.csv',1);
+%d5 = csvread('wisent/50cm/result.csv',1);
+d6 = csvread('wisent_random/60cm/result.csv',1);
 
 % Reshape matrices.
 r2 = reshape(d2,5,16,17);
-r3 = reshape(d3,5,16,17);
+%r3 = reshape(d3,5,16,17);
 r4 = reshape(d4,5,16,17);
-r5 = reshape(d5,5,16,17);
+%r5 = reshape(d5,5,16,17);
 r6 = reshape(d6,5,16,17);
 
 fontSize = 10;
@@ -42,32 +42,34 @@ hFig = figure;
 set(hFig, 'Position', figurePosition)
 set(gcf,'Renderer','painters');
 
-data = [r2(:,:,s),r3(:,:,s),r4(:,:,s),r5(:,:,s),r6(:,:,s)];
+%data = [r2(:,:,s),r3(:,:,s),r4(:,:,s),r5(:,:,s),r6(:,:,s)];
+data = [r2(:,:,s),r4(:,:,s),r6(:,:,s)];
 
 % Boxplot grouping stuff.
 a = [];
-for j=1:5,
+for j=1:3,
     a = [a 1:16];
 end
-l = [repmat({'20'},1,16), repmat({'30'},1,16),repmat({'40'},1,16),repmat({'50'},1,16),repmat({'60'},1,16)];
+%l = [repmat({'20'},1,16), repmat({'30'},1,16),repmat({'40'},1,16),repmat({'50'},1,16),repmat({'60'},1,16)];
+l = [repmat({'20'},1,16),repmat({'40'},1,16),repmat({'60'},1,16)];
 
 % Boxplot.
-b = boxplot(data,{a,l},'colors',repmat('rbgmk',1,16),'factorgap',[5 2], ...
+b = boxplot(data,{a,l},'colors',repmat('rbg',1,16),'factorgap',[3 2], ...
 'labelverbosity','minor', 'factorseparator',1);
 set(findobj(get(b(1), 'parent'), 'type', 'text'), 'FontSize', fontSize, ...
     'FontWeight', fontWeight);
 
 % Recolor boxes. 
-colors = repmat('kmgbr',1,16);
+colors = repmat('gbr',1,16);
 h = findobj(gca,'Tag','Box');
 for j=1:length(h)
    patch(get(h(j),'XData'),get(h(j),'YData'), colors(j),'FaceAlpha',1,'EdgeColor','none');
 end
 
 delete(findobj(gca,'Type','text','String','20'));
-delete(findobj(gca,'Type','text','String','30'));
+%delete(findobj(gca,'Type','text','String','30'));
 delete(findobj(gca,'Type','text','String','40'));
-delete(findobj(gca,'Type','text','String','50'));
+%delete(findobj(gca,'Type','text','String','50'));
 delete(findobj(gca,'Type','text','String','60'));
 
 h = xlabel('Message payload size (words)');
@@ -75,13 +77,14 @@ set(h,'FontSize',fontSize);
 set(h,'fontweight', fontWeight);
 
 
-h = ylabel('Throughput [B/sec]');
+h = ylabel('Runtime [sec]');
 set(h,'FontSize',fontSize);
 set(h,'fontweight', fontWeight);
-
+ylim([0 inf])
 
 % Turn on legend.
-h = legend(findobj(gca,'Tag','Box'),'60 cm','50 cm','40 cm','30 cm','20 cm', 'Location','northwest');
+%h = legend(findobj(gca,'Tag','Box'),'60 cm','50 cm','40 cm','30 cm','20 cm', 'Location','northwest');
+h = legend(findobj(gca,'Tag','Box'),'60 cm','40 cm','20 cm', 'Location','northwest');
 set(h,'FontSize',fontSize);
 set(h,'fontweight', fontWeight);
 
