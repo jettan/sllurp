23b0da3de2eb97d78bb3c14a8b65a0239979b75d
Jethro Tan <jethrotan20@gmail.com>
2015-07-27 16:14:12 +0200

    Update datalogging script.

--- get_wisent_data.sh
+++ get_wisent_data.sh
@@ -6,7 +6,7 @@ sleep 5
 
 for i in {16..1}; do
 	for j in {1..5}; do
-		./bin/access -P 32 -w 1 -c 0xb105 -m ${i}  -f hex/test_${i}.hex -M WISP5 -T 6250 -n 200 -l log${i}_${j}.txt 192.168.10.102
+		./bin/wisent -f hex/test_${i}.hex -l log${i}_${j}.txt 192.168.10.102
 		sleep 5
 	done
 	./bin/reset 192.168.10.102
